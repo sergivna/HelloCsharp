a4bb38b77a668fc7219a5c9db5dfcf3e839010c5
Julia <Julia@DESKTOP-5OVO5OF>
2018-07-08 22:46:39 +0300

    finished

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -8,7 +8,22 @@ namespace Lab
 {
     class Program
     {
-         static int getCont(ref int a)
+        static void Main(string[] args)
+        {
+            int a = 0;
+            GetCont(ref a);
+            if (a < 0 || a == 0)
+            {
+                Console.WriteLine("<<<WRONG PARAMETR>>>");
+                Console.ReadKey();
+                return;
+            }
+            ProcessPoint(a);
+            Console.ReadKey();
+        }
+
+
+        public static int GetCont(ref int a)
         {
             
             Console.WriteLine("ВВедіть параметр а (а>0)");
@@ -17,7 +32,7 @@ namespace Lab
             else return a;
         }
 
-        static void getPoint (ref double x,ref double y)
+         public  static void GetPoint (ref double x,ref double y)
         {
             Console.WriteLine("Введіть x:");
             x = Convert.ToDouble(Console.ReadLine());
@@ -25,17 +40,17 @@ namespace Lab
             y = Convert.ToDouble(Console.ReadLine());
         }
 
-        static void outCont( int a)
+         public static void OutCont( int a)
         {
-            Console.WriteLine($"[{a}]");
+            Console.WriteLine($"Parametr is [{a}]");
         }
         
-        static void outPoint(double x, double y)
+        public static void OutPoint(double x, double y)
         {
-            Console.WriteLine($"({x};{y})");
+            Console.WriteLine($"Point is ({x};{y})");
         }
 
-        static int  wherePoint (int a, double x, double y)
+        public static int  WherePoint (int a, double x, double y)
         {
             if (x < 0) x = -x;
             if (y < 0) y = -y;
@@ -45,30 +60,21 @@ namespace Lab
             
         }
 
-        static void processPoint ( int a)
+        public static void ProcessPoint ( int a)
         {
             double x = 0, y = 0;
-            getPoint(ref x,ref y);
-            outCont(a);
-            outPoint(x, y);
-
-
-
+            GetPoint(ref x,ref y);
+            OutCont(a);
+            OutPoint(x, y);
+           
+            if (WherePoint(a,x,y)==-1)
+                Console.WriteLine("OUT_C");
+            if (WherePoint(a, x, y) == 0)
+                Console.WriteLine("ON_C");
+            if (WherePoint(a, x, y) == 1)
+                Console.WriteLine("IN_C");
         }
 
-        static void Main(string[] args)
-        {
-            int a=0, x, y;
-            getCont(ref a);
-            if (a<0 || a==0)
-            {
-                Console.WriteLine("<<<WRONG PARAMETR>>>");
-                return;
-            }
-            processPoint(a);
-
-            Console.WriteLine(a);
-            Console.ReadKey();
-        }
+        
     }
 }
